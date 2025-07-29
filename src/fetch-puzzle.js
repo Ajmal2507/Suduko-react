25a1990327ed8bab3491f1f7b654bc98e5527786
ajmal k <ajmal25k@gmaiil.com>
2025-07-29 18:34:28 +0530

    Tnitial commit

---
+++ src/fetch-puzzle.js
@@ -0,0 +1,35 @@
+const SUDOKU_API = 
+  'https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,solution}}}'
+
+
+export const fetchPuzzle = async ({
+    setError,
+    setStatus,
+    setPuzzle,
+    setSolution,
+    setBoard,
+    setSelected
+}) => {
+    setError('')
+    setStatus('')
+    
+    try{
+        const res = await fetch(SUDOKU_API);
+        const data = await res.json();
+
+        const grid = data.newboard.grids[0];
+        const Puzzle = grid.value.map((row) => 
+        row.map((cell) => (cell === 0 ? null : cell)));
+        const solution = grid.solution.map((row) => 
+        row.map((cell) => (cell === 0 ? null : cell)));
+
+        setPuzzle(Puzzle);
+        setSolution(solution);
+        setBoard(Puzzle.map((row) => [...row]));
+        setSelected(null);
+    }
+        catch(e){
+            setError("Failed to fetch Puzzle",e);
+        }
+    
+}
